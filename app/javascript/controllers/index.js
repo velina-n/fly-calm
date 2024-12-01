d8273cf2cb83a0757b3ca1f65c221c9cbcc7762d
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-12-01 12:09:45 +0100

    echec

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,4 @@
-// app javascript favorites_controller.js
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-console.log("Chargement des contrôleurs Stimulus...");
-eagerLoadControllersFrom("controllers", application);
-console.log("Tous les contrôleurs ont été chargés !");
+eagerLoadControllersFrom("controllers", application)
