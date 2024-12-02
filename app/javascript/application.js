18e36f802e790e269c17b6f972d9e42dea4e9727
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-12-02 10:26:44 +0100

    validation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,8 @@
 // app javascript application.js
-// app/javascript/application.js
-import { Application } from "@hotwired/stimulus";
-import "@hotwired/turbo-rails"; // Pour la navigation dynamique (Hotwire)
-import "controllers"; // Charge automatiquement les contrôleurs Stimulus
-import "bootstrap"; // Charge Bootstrap pour les composants interactifs
-
+import "@hotwired/turbo-rails"
+import "controllers"
+import "@popperjs/core"
+import "bootstrap"
 // Import d'animations personnalisées (si elles existent)
 import "../animations/circle_animation";
 import "../animations/bar_animation";
-
-// Débogage pour vérifier que ce fichier est chargé
-console.log("Application.js chargé avec succès !");
-
-// Initialise Stimulus
-const application = Application.start();
-application.debug = false; // Désactiver les logs en production
-window.Stimulus = application;
-
-export default application;
