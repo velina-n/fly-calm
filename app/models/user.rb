e9f04d9f0fad2278571fbb2c6d9fff5b46a32564
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-12-02 17:44:38 +0100

    after sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorite_documents, through: :favorites, source: :document
+  has_many :journeys, dependent: :destroy
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates :email, presence: true, uniqueness: true
